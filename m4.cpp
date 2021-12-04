b4d9d8e3275f877cf6d79186db25334691283cc4
Joyonghihae <rladms010@gmail.com>
2021-12-03 23:52:35 -0800

    m4.cpp

--- m4.cpp
+++ m4.cpp
@@ -40,16 +40,22 @@ int main() {
 
 			return -3;
 		}
-		
-		
-		return 0;
 	}
+
+	return 0;
 }
 int processGames(char fileName[]){
 
-	//opens files, outputs values?????, lists them in order of size, and calculates them.
 	FILE* fp1 = NULL;
 	char leng01[40] = { 0 };
+	int test02 = 0;
+	char blbl[40] = { 0 };
+	double score01 = 0;
+	double score02 = 0;
+	double score03 = 0;
+	int win = 0;
+	int lost = 0;
+	int tied = 0;
 	strcpy(leng01, fileName);
 	fp1 = fopen(leng01, "r");
 	if (fp1 == NULL) {
@@ -68,44 +74,40 @@ int processGames(char fileName[]){
 
 			return -2;
 		}
-		printf("This is check line: %s\n", leng01);
 		char number[40] = { 0 };
 		char name[40] = { 0 };
 		int scoreNumber = 0;
 		int opponent = 0;
 		strcpy(number, leng01);
 		parseLine(number, name, &scoreNumber, &opponent);
-		//// (2 * wins + ties) /(2 * (wins + losses + ties))
-
-		/*
-		int test = 0;
-		int count = 0;
-		int score = 0;
-		for (int i = 0; leng01[i]; i++) {
-
-			if (leng01[i] >= '0' && leng01[i] <= '9') {
-				test = leng01[i] - '0';
-				printf("This is number: %d\n", test);
-				count++;
-				if (count == 2) {
-					if (score > test) {
-						printf("이겼다\n");
-					}
-					else if (score < test) {
-						printf("졌다?\n");
-					}
-					else if (score == test) {
-						printf("비겼네?\n");
-					}
-				}
-				score = test;
+		
+		for (int i = 0; i < fileName[i]; i ++ ){
+			if ((fileName[i] >= 'A' && fileName[i] <= 'Z') || (fileName[i] >= 'a' && fileName[i] <= 'z') ||  fileName[i] == ' ') {
+				test02 = fileName[i];
+				blbl[i] = test02;
 			}
-		}*/
-		//char* ptr = strchr(number, ',')+1;
-		//printf("Finally%s\n", ptr);
-
-
+			
+		}
+		
+		if (scoreNumber > opponent) {
+			printf("\t%s best %s %d-%d\n", blbl, name,scoreNumber,opponent);
+			score01 += scoreNumber;
+			win++;
+		}
+		else if (scoreNumber < opponent) {
+			printf("\t%s lost to %s %d-%d\n", blbl, name,opponent,scoreNumber);
+			score02 += scoreNumber;
+			lost++;
+		}
+		else if (scoreNumber == opponent) {
+			printf("\t%s and %s tied at %d\n", blbl, name,scoreNumber);
+			score03 += scoreNumber;
+			tied++;
+		}
 	}
+	double result = 0;
+	result = (2 * (score01 + score03)) / (2 * (score01 + score02 + score03));
+	printf("Seasn result for %s: %.3f (%d-%d-%d)\n ", blbl, result,win,lost,tied);
 
 	if (fclose(fp1) != 0) {
 		// the closing of the file failed
@@ -118,23 +120,9 @@ int processGames(char fileName[]){
 	
 	return 0;
 	}
-	/*
-	문자열에서 숫자만 가져와서 비교 -> 크기순으로 다시정렬해서 출력
-	
-	*/
-	//여기서 parseling호출 
-	//여기서 전체 점수 결과  
 
-// return int???? void???? ??????????????? 
+
 int parseLine(char resultArray[], char nameArray[], int *pScore, int *pOpponent) {
-	//게임 결과줄을 분석해야함 
-	/*
-	*   Arizona,3-2
-		St. Louis,2-2
-		New York Rangers,4-5
-	*/
-	// 게임 결과 문자열 :Toronto beat Winnipeg 4-2 / 상대팀만뽑아온 배열 / 1차팀 점수 포인터/ 상대팀 점수 포인
-	// , = 44
 	int test = 0;
 	int count = 0;
 	int test01 = 0;
@@ -149,27 +137,17 @@ int parseLine(char resultArray[], char nameArray[], int *pScore, int *pOpponent)
 		
 		if (resultArray[i] >= '0' && resultArray[i] <= '9') {
 			test = resultArray[i] - '0';
-			printf("This is number: %d\n", test);
 			count++;
 			if (count == 1) {
 			*pScore = test;
 			}else if (count == 2) {
-				if (*pScore > test) {
-					printf("win!\n"); //return this?
-				}
-				else if (*pScore < test) {
-					printf("lost!\n");
-				}
-				else if (*pScore == test) {
-					printf("tied\n");
-				}
+				
 				*pOpponent = test;
 			} 
 		}
 
 	}
 
-	printf("finally.... %s\n", nameArray);
 	 
 	return 0;
 }
